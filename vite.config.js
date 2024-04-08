fc85b71965c40ce82e95d7fac42ce1188e7a89c9
just_a_mango <horace.hoff@gmail.com>
2024-04-08 13:28:29 +0200

    /

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,27 @@ export default defineConfig({
         transformer: "lightningcss",
         lightningcss: {
             targets: browserslistToTargets(browserslist('>= 0.25%')),
-        }
-    }, build: {
+        },
+        devSourcemap: false
+    },
+    build: {
         cssMinify: 'lightningcss',
-        minify: "terser"
-    }
+        minify: "terser",
+        terserOptions: {
+            sourceMap: false,
+            compress: {
+                passes: 1000,
+                drop_console: true,
+                drop_debugger: true,
+                toplevel: true,
+            }
+        },
+        rollupOptions: {
+            treeshake: "smallest",
+            output: {
+                compact: true,
+            }
+        },
+        sourcemap: false
+    },
 })
